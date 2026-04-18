77a0ec5a4e2897cf35938c7a7c76fa1041ef6a0b
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2026-04-19 00:11:03 +0400

    Added linkedin link and package log changes

--- src/MainPageSections/WelcomeSection.vue
+++ src/MainPageSections/WelcomeSection.vue
@@ -1,86 +1,93 @@
 <script setup>
 import PageSection from "@/PageSection.vue";
-import { Github, Youtube, ArrowDown } from "@iconoir/vue";
+import { Github, Youtube, Linkedin, ArrowDown } from "@iconoir/vue";
 
 const emit = defineEmits(["scroll-down"]);
 </script>
 
 <template>
-  <PageSection>
-    <div class="main-bg"></div>
+    <PageSection>
+        <div class="main-bg"></div>
 
-    <div class="floating-hint-top" @click="emit('scroll-down')">
-      <ArrowDown class="undulate-anim" /> Scroll down
-    </div>
+        <div class="floating-hint-top" @click="emit('scroll-down')">
+            <ArrowDown class="undulate-anim" /> Scroll down
+        </div>
 
-    <div class="flex-page flex-page-centered">
-      <div class="content-width">
-        <h1 class="big-title marginb italic accent text-solid-shadow">
-          Welcome.
-        </h1>
-        <p class="italic">
-          I'm Vardan, a 16-year-old computer enthusiast and self-taught
-          programmer from Armenia.
-        </p>
-        <hr />
+        <div class="flex-page flex-page-centered">
+            <div class="content-width">
+                <h1 class="big-title marginb italic accent text-solid-shadow">
+                    Welcome.
+                </h1>
+                <p class="italic">
+                    I'm Vardan, a 16-year-old computer enthusiast and
+                    self-taught programmer from Armenia.
+                </p>
+                <hr />
 
-        <a
-          class="marginb link-btn square"
-          target="_blank"
-          href="https://github.com/Vardan2009"
-          ><Github />
-        </a>
+                <a
+                    class="marginb link-btn square"
+                    target="_blank"
+                    href="https://github.com/Vardan2009"
+                    ><Github />
+                </a>
 
-        <a
-          class="marginb link-btn square"
-          target="_blank"
-          href="https://youtube.com/@vpetrosyan"
-          ><Youtube />
-        </a>
-      </div>
-    </div>
-  </PageSection>
+                <a
+                    class="marginb link-btn square"
+                    target="_blank"
+                    href="https://youtube.com/@vpetrosyan"
+                    ><Youtube />
+                </a>
+
+                <a
+                    class="marginb link-btn square"
+                    target="_blank"
+                    href="https://linkedin.com/in/vpetrosyan5"
+                    ><Linkedin />
+                </a>
+            </div>
+        </div>
+    </PageSection>
 </template>
 
 <style scoped>
 .main-bg {
-  position: absolute;
-  inset: 0;
-  opacity: 0.25;
-  background-image: url(/pattern.svg);
+    position: absolute;
+    inset: 0;
+    opacity: 0.25;
+    background-image: url(/pattern.svg);
 
-  background-size: cover;
+    background-size: cover;
 
-  background-size: 100px;
+    background-size: 100px;
 
-  z-index: -1;
+    z-index: -1;
 
-  background-attachment: fixed;
+    background-attachment: fixed;
 
-  -webkit-mask-image: linear-gradient(to bottom, black 70%, transparent 100%);
-  mask-image: linear-gradient(to bottom, black 70%, transparent 100%);
+    -webkit-mask-image: linear-gradient(to bottom, black 70%, transparent 100%);
+    mask-image: linear-gradient(to bottom, black 70%, transparent 100%);
 
-  animation: bg-slide 150s linear infinite;
+    animation: bg-slide 150s linear infinite;
 }
 
 .main-bg::after {
-  content: " ";
-  background-image: linear-gradient(
-    to bottom,
-    #e6a84a99 0%,
-    rgba(0, 0, 0, 0) 70%
-  );
-  position: absolute;
-  inset: 0;
-  z-index: 1;
+    content: " ";
+    background-image: linear-gradient(
+        to bottom,
+        #e6a84a99 0%,
+        rgba(0, 0, 0, 0) 70%
+    );
+    position: absolute;
+    inset: 0;
+    z-index: 1;
 }
 
 @keyframes bg-slide {
-  from {
-    background-position: 0 0;
-  }
-  to {
-    background-position: -100% 0;
-  }
+    from {
+        background-position: 0 0;
+    }
+    to {
+        background-position: -100% 0;
+    }
 }
 </style>
